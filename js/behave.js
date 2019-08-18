881728404127f41c7d96eb12f15614fa5c1ee206
lakhyajd <ljd.develop@outlook.com>
2019-08-18 19:24:20 +0100

    commit

--- js/behave.js
+++ js/behave.js
@@ -5,21 +5,25 @@ var actAttempts = 1;
 
 function process() {
     var userNumber = document.getElementById("inputbox").value;
-    if (userNumber < 11) {
+    if (userNumber > 0) {
+        if (userNumber < 11) {
 
-        if (totAttempts != actAttempts) {
-            if (userNumber == number) {
+            if (totAttempts != actAttempts) {
+                if (userNumber == number) {
 
-                document.getElementById("outbox").innerHTML = "Horrray. Thats a match! You did in " + actAttempts + " attempts";
+                    document.getElementById("outbox").innerHTML = "Horrray. Thats a match! You did in " + actAttempts + " attempts";
+                } else {
+                    document.getElementById("outbox").innerHTML = "Oops. Incorrect guess. Try again! " + (totAttempts - actAttempts) + " attempts left.";
+                    actAttempts++;
+
+                }
             } else {
-                document.getElementById("outbox").innerHTML = "Oops. Incorrect guess. Try again! " + (totAttempts - actAttempts) + " attempts left.";
-                actAttempts++;
-
+                document.getElementById("outbox").innerHTML = "All attempts are over. Please refresh this page and try again.";
             }
         } else {
-            document.getElementById("outbox").innerHTML = "All attempts are over. Please refresh this page and try again.";
+            document.getElementById("outbox").innerHTML = "Please enter a number between 1 and 10 only."
         }
     } else {
-        document.getElementById("outbox").innerHTML = "Please enter a number between 1 and 10 only."
+        document.getElementById("outbox").innerHTML = "Please enter a number to proceed"
     }
 }
